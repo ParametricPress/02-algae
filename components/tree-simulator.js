25aafc666b3f34d598299406b351ce3cabc3afb8
Ben Cooley <benjamincooley94@gmail.com>
2020-09-25 16:30:58 -0400

    sim updates

--- components/tree-simulator.js
+++ components/tree-simulator.js
@@ -20,78 +20,28 @@ class treeSimulator extends Component {
   };
 
   updateTrees = e => {
-    this.branchList = this.createBranches(this.NUM_OF_BRANCHES);
-    this.setState({ branches: this.branchList });
+    // this.branchList = this.createBranches(this.NUM_OF_BRANCHES);
+    // this.setState({ branches: this.branchList });
+    d3.selectAll(".svg-trees").remove();
+      
+    for (let i = 0; i < this.props.numtrees; i++) {
+      let randX = Math.floor(Math.random()*399) + 1;
+      let randY = (Math.floor(Math.random()*49) + 1)+70;
+      this.drawSvgTree(randX, randY)  
+    }
   };
 
-  setColor(r) {
-    //hue based on r : 0 -60 red to yellow
-    var s = 10;
-    var l = 100;
-    var c = this.hslToHex(r * 2, s, l);
-    return c;
-  }
+  drawSvgTree(x, y) {
+    const width = 600,
+      height = 300;
 
-  createBranches(num) {
-    var treeHeight = 6;
-    var deg_to_rad = Math.PI / 180.0;
-    var depth = 5;
-    let length= treeHeight + Math.random()*2;
-    let branches = [];
-    let _self=this;
-    function drawTree(x1, y1, angle, depth){
-        angle+=10-Math.random()*15;
-
-      if (depth !== 0){
-        var x2 = x1 + (Math.cos(angle * deg_to_rad) * depth * length);
-        var y2 = y1 + (Math.sin(angle * deg_to_rad) * depth * length);
-        let color=_self.setColor(12+(2*depth));
-        // let color=_self.setColor(Math.random()*2*depth);
-        let branch={x1:x1,y1: y1, x2:x2, y2:y2, depth:depth,color:color};
-        branches.push(branch);
-
-        drawTree(x2, y2, angle - 25, depth - 1);
-        drawTree(x2, y2, angle , depth - 1);
-        drawTree(x2, y2, angle + 25, depth - 1);
-      }
-    }
-
-    let angle=-100+Math.random()*25;
-
-    for (let i = 0; i < this.props.numtrees; i++) {
-      drawTree((Math.random() * 200) + 5, 100, angle, depth);
-    }   
-    
-    return branches;
-  }
-  
-  hslToHex(h, s, l) {
-    h /= 360;
-    s /= 100;
-    l /= 100;
-    let r, g, b;
-    if (s === 0) {
-      r = g = b = l; // achromatic
-    } else {
-      const hue2rgb = (p, q, t) => {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-      };
-      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-      const p = 2 * l - q;
-      r = hue2rgb(p, q, h + 1 / 3);
-      g = hue2rgb(p, q, h);
-      b = hue2rgb(p, q, h - 1 / 3);
-    }
-    const toHex = x => {
-      const hex = Math.round(x * 255).toString(16);
-      return hex.length === 1 ? '0' + hex : hex;
-    };
-    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+    d3.select("#treelandscape").append("svg:image")
+      .attr("xlink:href", "static/images/svg-tree-stroke.svg")
+      .attr("class", "svg-trees")
+      .attr("width", 60)
+      .attr("height", 60)
+      .attr("x", (width/6) + x)
+      .attr("y",height - y);
   }
 
   componentDidMount() {
@@ -101,8 +51,8 @@ class treeSimulator extends Component {
   }
 
   startNodeSimulation() {
-    const width = 700,
-      height = 400,
+    const width = 600,
+      height = 300,
       radius = 2;
 
     let nodes = [],
@@ -115,7 +65,7 @@ class treeSimulator extends Component {
 
     let simulation = d3.forceSimulation()
           .force("x", d3.forceX().x(width/2))
-          .force("y", d3.forceY().y(160))
+          .force("y", d3.forceY().y(80))
           .force("gravity", d3.forceManyBody().strength(-radius - 2))
           .force("collide", d3.forceCollide()
               .radius(d => d.radius + 0.1)
@@ -124,14 +74,15 @@ class treeSimulator extends Component {
           .on("tick", () => {
             svg.selectAll("circle")
               .attr("cx", d => d.x)
-              .attr("cy", d => d.y);
+              .attr("cy", d => d.y)
+              // .attr("stroke", "#D9FFA2")
           });
 
     startSimulation();
 
     function startSimulation() {
       int = d3.interval(() => {
-            addNode();
+            addNode("red");
             updateSim();
             if (counter > 150) {
                 int.stop();
@@ -139,13 +90,19 @@ class treeSimulator extends Component {
         }, 30);
     }
 
-    function addNode() {
+    function addNode(color) {
+      let cfill;
+      if (color == "blue") {
+        cfill = "#9BBBD8"
+      } else {
+        cfill="#F09989"
+      }
         nodes.push(
             {
                 radius,
                 x: width/2,
                 y: height/1.5,
-                fill: '#D9FFA2'
+                fill: cfill
             }
         );
 
@@ -181,7 +138,7 @@ class treeSimulator extends Component {
               d3.select("#treelandscape")
                   .append('rect')
                   .attr("x", 270+(this.state.yearCount*10))
-                  .attr("y", 395 - (i*10))
+                  .attr("y", 255 - (i*10))
                   .attr('rx', 10)
                   .attr('ry', 10)
                   .attr('width', 5)
@@ -211,7 +168,7 @@ class treeSimulator extends Component {
           let counterForTrees = setInterval(() => {
             // for every tree killed, release all carbon from tree
             for (let i = 0; i < this.state.yearCount * 1; i++) {
-              addNode();
+              addNode("blue");
               
               // remove # of carbon that tree has sequestered
               d3.select("#treelandscape").select('.removed-carbon')
@@ -290,55 +247,44 @@ class treeSimulator extends Component {
   }
   componentDidUpdate(prevProps) {
     if (this.props.numtrees !== prevProps.numtrees) {
-      
       this.updateTrees()
-       
     }
   }
   render() {
-    const viewHeight = 25;
+    const viewHeight = 15;
     return (
-      <div className="app row">
-        <div className="column sim-controls">
-          <div class='progress-bar-outer'>
-            <div class='progress-bar-inner' style={{width: (((this.props.numtrees - 1) / (this.props.maxTrees - 1))*100) + '%'}}></div>
-          </div>
-          <p>Trees in forest: {this.props.numtrees}</p>
-
-          <div class='progress-bar-outer'>
-              <div class='progress-bar-inner' style={{width: (this.state.yearCount * 3.333) + '%'}}></div>
-          </div>
-          <p>Years elapsed: {this.state.yearCount}</p>
-          
-          <div class='progress-bar-outer'>
-              <div class='progress-bar-inner' style={{width: ((this.state.disasterCount*10)) + '%'}}></div>
-          </div>
-          <p>Deforestation events: {this.state.disasterCount}</p>
-          
-          <button id="start-btn">Start Simulation</button>
-          { this.state.simulationState == 'waitingForRestart'  ? <button id="restart-btn" onClick={() => this.resetTheSim()}>Reset</button> : null }
+      <div className="app row tree-sim">
+        <div>
+          { this.state.simulationState == 'beforeFirstStart'  ? <h4>Let's plant a forest of { this.props.numtrees * 1000 } trees</h4> : null }
+          { this.state.simulationState == 'beforeFirstStart'  ? <p>Each 🌲 represents 1,000 trees</p> : null }
         </div>
-        
+        <div >
+          { this.state.simulationState == 'running' || this.state.simulationState == 'waitingForRestart' ? <h4>{this.state.simulationMessage}</h4> : null }
+          { this.state.simulationState == 'running' || this.state.simulationState == 'waitingForRestart'  ? <p>{this.state.disasterCount} deforestation events </p> : null }
+        </div>
+      
         <div className="content column">
           <svg id="treelandscape" xmlns="http://www.w3.org/2000/svg">
-            <rect class='tooltip-box' x='150' y='10' width='400' height='60' rx='10' ry='10'></rect>
-            <text class='tooltip-text' x='340' y='45'>
-              {this.state.simulationMessage}
-            </text>
-        
-            {this.state.branches.map((b, i) => (
-              <g id="svg_1" 
-              key={i}
-              transform="translate(260,300)"
-              >             
-             <line id="svg_2" x1={b.x1} y1={b.y1} x2={b.x2} y2={b.y2}
-                  strokeWidth=".5"
-                  stroke={b.color}
-                 
-                />
-              </g>))}
+            <ellipse cx="440" cy="7" rx="3" fill="#F09989"></ellipse>
+            <text class="co2-legend" x="450" y="10">= 10kg CO₂</text>
+            <text class="co2-type" x="250" y="10">Atmospheric CO₂</text>
+            <text class="co2-type" x="250" y="290">Sequestered CO₂</text>
           </svg>
         </div>
+
+        <div className="flex-container">
+          <div class="flex-child">
+            <button id="start-btn">Start Simulation</button>
+            { this.state.simulationState == 'waitingForRestart'  ? <button id="restart-btn" onClick={() => this.resetTheSim()}>Reset</button> : null }
+          </div>
+          <div class="flex-child-3">
+            <div class='progress-bar-outer'>
+                <div class='progress-bar-inner' style={{width: (this.state.yearCount * 3.333) + '%'}}></div>
+            </div>
+            <p>Years elapsed: {this.state.yearCount}</p>
+          </div>
+        </div>
+
         <div>
         <span class="simulation-citation">The data for this simulation is based on a few estimates, but is mainly meant to illustrate a wider problem. Rate of CO₂ removal assumes that a mature tree absorbs 48 lbs/yr (estimate from the <a href="http://www.tenmilliontrees.org/trees/" target="_blank">10 Million Trees project</a>), but these estimates vary greatly by region, type of tree, and age of tree. The rate of deforestation assumes that deforestation rates will stay the same or increase in the future.</span>
         </div>
